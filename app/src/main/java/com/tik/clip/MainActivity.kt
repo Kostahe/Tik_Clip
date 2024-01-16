1a9bc5d4bf45855cf1bba7737d3fb3018695cdbd
nooooooootbuk <quertical5@gmail.com>
2024-01-16 14:49:56 +0100

    Added Scaffold containing BottomAppBar with IconButton (test)

--- app/src/main/java/com/tik/clip/MainActivity.kt
+++ app/src/main/java/com/tik/clip/MainActivity.kt
@@ -3,13 +3,21 @@ package com.tik.clip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.tik.clip.ui.theme.TikClipTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +30,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        bottomBar = {
+                            BottomAppBar {
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(
+                                        imageVector = Icons.Default.Home,
+                                        "Test")
+                                }
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(
+                                        imageVector = Icons.Default.MailOutline,
+                                        "Test")
+                                }
+                            }
+                        }
+                    ) {
+                        Text(text = "Hi", modifier = Modifier.padding(it))
+                    }
                 }
             }
         }
+
     }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TikClipTheme {
-        Greeting("Android")
-    }
-}
